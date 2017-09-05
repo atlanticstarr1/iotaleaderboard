0deb1949cc9feadc95649e8fe84e74ff88c8e142
Noel,David <atlanticstarr@ufl.edu>
2017-09-05 00:57:47 -0400

    Iota leaderboard

---
+++ IotaSandbox/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IotaSandbox.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+        public IActionResult Receive()
+        {
+            ViewData["Message"] = "Leader board.";
+
+            return View();
+        }
+        public IActionResult Send()
+        {
+            ViewData["Message"] = "Send Iota.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
